7aae39f0b189f8016ad4f853fa40db2caffbf1a4
sourabhpatole <sourabhpatole@outlook.com>
2025-07-23 16:29:31 +0530

    modified route and outlet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,25 @@ import Loading from "./components/common/Loading";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/protected/Home";
 import Search from "./pages/protected/Search";
-import Edit from "./pages/protected/Edit";
+import Edit from "./pages/protected/ProtectedLayout";
+import Error from "./pages/Error";
+import Register from "./pages/Register";
+import { Box } from "@mui/material";
+import ProtectedLayout from "./pages/protected/ProtectedLayout";
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/search" element={<Search />} />
-          <Route path="/edit" element={<Edit />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </BrowserRouter>
+      <Box minHeight={"100vh"}>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<ProtectedLayout />}>
+              <Route exact path="" element={<Home />} />
+              <Route exact path="post/:id" element={<h1>Single Post</h1>} />
+              <Route exact path="search" element={<Search />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Box>
     </>
   );
 };
--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 import React from "react";
 
 const Error = () => {
@@ -8,15 +8,32 @@ const Error = () => {
         width={"100%"}
         height={"100vh"}
         flexDirection={"row"}
-        justifyContent={"center"}
+        justifyContent={"flex-start"}
         alignItems={"center"}
         sx={{
-          background: "url",
+          background: 'url("/error-bg.jpg")',
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
         }}
       >
-        <Stack>
+        <Stack
+          p={5}
+          border={"2px solid black"}
+          marginLeft={"20px"}
+          borderRadius={"10px"}
+          flexDirection={"column"}
+          alignItems={"center"}
+          gap={2}
+          boxShadow={"7px 7px 7px white"}
+        >
           <Typography variant="h3">OOP's</Typography>
           <Typography variant="h6">You are on Wrong Page!</Typography>
+          <Button
+            size="large"
+            sx={{ bgcolor: "blue", color: "white", borderRadius: "10px", p: 2 }}
+          >
+            Go Back
+          </Button>
         </Stack>
       </Stack>
     </div>
